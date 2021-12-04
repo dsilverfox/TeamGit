9d71cdef72db3a6731e16d7388eace4b94cb9f0b
Devon Silverfox-Patchett <DE.Silverfox.Coding@gmail.com>
2021-12-04 15:57:26 -0500

    commiting for pull.

--- 72-hr-client/src/App.js
+++ 72-hr-client/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+
 import './App.css';
+import './Weather/Weather';
+// import './Events/Events';
+// import './satellite/Satellite';
 
 function App() {
+  const [location, setLocation] = useState('');
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+
+
+
+    const geoLocation = () => {
+      if (navigator.geolocation) {
+      console.log(geoLocation)
+      // navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+      console.log("Geolocation is not supported by this browser.");
+    }
+
+    geoLocation();
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
     </div>
   );
 }
