776aadcbb92ab85cacf0e07d20166b942142837a
Danielle Joy <daniellejoy@danielles-mbp.lan>
2021-12-06 11:48:26 -0500

    finally working api call

--- 72-hr-client/src/Events/Events.js
+++ 72-hr-client/src/Events/Events.js
@@ -1,51 +1,41 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, Suspense} from 'react';
+import { Button, Container, Card, CardContent, CardMedia, Typography, CardActionArea, CardActions } from '@mui/material';
+
+const Events = (props) => {
+    const key = 'FnaYTDcosjKQCuUiXFfwUinwdIVYh0ag';
+    // const geohash = props.geoHash
 
 
-const Events = () => {
-
-    const [latitude, setLatitude] = useState("");
-    const [longitude, setLongitude] = useState("");
-    const [locationDataFound, setLocationDataFound] = useState(false);
+    const [events, setEvents] = useState([])
     
-  
-    useEffect(() => {
-      geoLocation();
-    }, []);
-  
-    const geoLocation = () => {
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-      } else {
-        console.log("Geolocation is not supported by this browser.");
-      }
-    };
-  
-    const showPosition = (position) => {
-      setLatitude(position.coords.latitude);
-      setLongitude(position.coords.longitude);
-    };
-  
-    useEffect(() => {
-      if (latitude && longitude) {
-        console.log("Latitude:", latitude, "Longitude:", longitude);
-        setLocationDataFound(true);
-      }
-    }, [latitude, longitude]);
-  
-    // useEffect(() => {
-    //   fetchEvents();
-    // });
+    
+    
 
-    // fetchEvents = () => {
-    //     console.log(position)
-    // }
+    const getEvents = async () => {
+       await fetch(`https://cors-anywhere.herokuapp.com/app.ticketmaster.com/discovery/v2/events.json?geoPoint=${props.geoHash}&apikey=${key}`, {
+            method: 'GET',
+            headers:new Headers({
+                "Content-type":'application/json',
+                "Access-Control-Allow-Origin":'*'
+            })
+        })
+        .then ((res) => res.json())
+        .then ((json) => setEvents(json))
+        .then (console.log(events))
 
-    // fetchEvents();
+}
+useEffect(() => { 
+    getEvents();
+}, [props.geoHash])
 
     return(
-       <div>
-           LOCATION
-       </div> 
-        )
+        <div>
+           <Suspense fallback={<div>Loading Events</div>}>
+            <Container>
+               EVENTS WILL GO HERE 
+            </Container>
+           </Suspense>
+           </div>
+    )
 }
     export default Events;
