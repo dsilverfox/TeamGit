9e8b63bc888c625af5e2960584e478e89978295c
Devon Silverfox-Patchett <DE.Silverfox.Coding@gmail.com>
2021-12-07 12:33:27 -0500

    updating styling

--- 72-hr-client/src/Weather/Weather.js
+++ 72-hr-client/src/Weather/Weather.js
@@ -1,7 +1,8 @@
  import React, { useEffect, useState } from 'react'; //useEffect
  import WeatherDisplay from './WeatherDisplay';
  import './Weather.css'
- 
+
+
  //material ui installed in TeamGit don't forget to import it if you use it.
  const Weather = props => {
      
@@ -9,8 +10,8 @@
      const baseURL = 'http://api.openweathermap.org/data/2.5/weather?';
      const key = '750ee8e3454df1bae3ec03c8ae23da35';
     // EXTRA KEY const key = '63dcb297673179ea05ba355cfa80fe06';
-    const [toggle, setToggle] = useState(false); 
-    const [units, setUnits] = useState("imperial");
+     const [toggle, setToggle] = useState(false); 
+     const [units, setUnits] = useState("imperial");
 
      const getWeather = async () => {
          const response = await fetch(`${baseURL}lat=${props.location[0]}&lon=${props.location[1]}&units=${units}&appid=${key}`);
@@ -35,20 +36,16 @@
 //end useEffect
 
      const changeUnit = () => {
-         (units === 'imperial') ? setUnits('metric') : setUnits('imperial') //requires two clicks and works once.
+         (units === 'imperial') ? setUnits('metric') : setUnits('imperial') //requires two clicks.
          getWeather();
-         console.log(units);
      }
-     console.log(weather)
 
     return (
-         <div>
+         <div className='loading'>
              {
                  toggle ? <WeatherDisplay weather={weather} changeUnit={changeUnit}/> : <p>No Data to Display Yet</p>
              }
-             <div>
-                 <button className='button' onClick={changeUnit}>Click me!</button>
-             </div>
+
          </div>
             )
  }
--- 72-hr-client/src/Weather/WeatherDisplay.js
+++ 72-hr-client/src/Weather/WeatherDisplay.js
@@ -3,16 +3,17 @@ import React from 'react'
 const WeatherDisplay = (props) =>{
 
     return(
-        <div>
+        <div className='display'>
         <h1>Weather Information</h1>
             <ul className='weatherInfo'>
-                    <li>{`The official temperature is ${props.weather.main.temp}. degrees.`}</li>
+                    <li>{`The official temperature is ${props.weather.main.temp} degrees.`}</li>
                     <li>{`However, to you it may feel ${props.weather.main.feels_like} degrees.`}</li>
                     <li>{`The temperature is supposed to get down to ${props.weather.main.temp_min} degrees.`}</li>
                     <li>{`The temperature will be as high as ${props.weather.main.temp_max} degrees.`} </li>
                     <li>{`The atmospheric pressure is ${props.weather.main.pressure} bars.`}</li>
                     <li>{`The humidity is ${props.weather.main.humidity}%`}</li>
                 </ul>
+            <button className='button' onClick={props.changeUnit}>Unit Toggle</button>
              </div>
     )};
 export default WeatherDisplay;
